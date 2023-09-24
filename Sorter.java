302bc079b8875fd8abf56c4c55384c768108c5f2
Fadel <fadel.a.alobaidi@gmail.com>
2023-09-24 22:54:31 +0300

    Insertion sort method has been added

--- Sorter.java
+++ Sorter.java
@@ -3,18 +3,16 @@ public class Sorter{
         
     }
 
-    private static void selectionSort(int[] array){
-        for(int i = 0; i < array.length - 1; i++){
-            int k = i;
-            for(int j = i + 1; j < array.length; j++){
-                if(array[j] < array[k])
-                    k = j;
+    void insertionSort(int arr[]) {
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
             }
-            if(k != i){
-                int min = array[k];
-                array[k] = array[i];
-                array[i] = min;
-            }
+            arr[j + 1] = key;
         }
     }
 }
